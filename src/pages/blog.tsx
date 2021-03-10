b8a47d8558c80ec6c9a85acf2beb0462ac48fe79
Bryan <bryanwyk@gmail.com>
2021-03-10 18:45:56 +1100

    [RF] Changed page title underline from gold to white

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -19,7 +19,7 @@ const Blog: FunctionComponent<{}> = ({ data }) => {
 				<h1
 					css={css`
 						text-align: center;
-						border-bottom: 3px solid ${darkTheme.colours.gold};
+						border-bottom: 3px solid ${darkTheme.colours.white};
 						height: 50px;
 						width: 135px;
 						margin: 0 auto;
--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -20,7 +20,7 @@ const Portfolio: FunctionComponent<{}> = ({ data }) => {
 				<h1
 					css={css`
 						text-align: center;
-						border-bottom: 3px solid ${darkTheme.colours.gold};
+						border-bottom: 3px solid ${darkTheme.colours.white};
 						height: 50px;
 						width: 270px;
 						margin: 0 auto;
