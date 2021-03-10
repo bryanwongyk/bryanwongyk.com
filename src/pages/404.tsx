dffb55285104d87f850aa36c296547d23c4bba29
Bryan <bryanwyk@gmail.com>
2021-03-10 16:39:09 +1100

    [RF] 404 styling added

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,20 @@
-import React, { FunctionComponent } from "react"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import React, { FunctionComponent } from 'react';
+import { css } from '@emotion/react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 const NotFoundPage: FunctionComponent<{}> = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+	<Layout>
+		<SEO title="404: Not found" />
+		<div
+			css={css`
+				text-align: center;
+			`}
+		>
+			<h1>404: Not Found</h1>
+			<p>This page doesn't exist. The sadness...</p>
+		</div>
+	</Layout>
+);
 
-export default NotFoundPage
+export default NotFoundPage;
