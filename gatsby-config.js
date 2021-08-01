3da8a96c5cc269564eb2a9ba54e07e7ea282f6ef
Bryan <bryanwyk@gmail.com>
2021-08-02 01:15:45 +1000

    Add 'Understanding JWTs' post

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,6 +54,27 @@ module.exports = {
 				icon: `src/content/assets/images/logo.png`, // This path is relative to the root of the site.
 			},
 		},
+
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-transformer-remark`,
+			options: {
+				plugins: [
+					{
+						resolve: `gatsby-remark-images`,
+						options: {
+							maxWidth: 800,
+						},
+					},
+				],
+			},
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				path: `${__dirname}/src/content/blog`,
+			},
+		},
 		// this (optional) plugin enables Progressive Web App + Offline functionality
 		// To learn more, visit: https://gatsby.dev/offline
 		// `gatsby-plugin-offline`,
