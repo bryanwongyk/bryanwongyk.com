9629614c989825ac75ea7efaae9df7c6770c17c5
Bryan <bryanwyk@gmail.com>
2021-08-15 15:00:21 +1000

    Canonical URL added

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,6 +75,12 @@ module.exports = {
 				path: `${__dirname}/src/content/blog`,
 			},
 		},
+		{
+			resolve: `gatsby-plugin-canonical-urls`,
+			options: {
+				siteUrl: `https://www.bryanwongyk.com/`,
+			},
+		},
 		// this (optional) plugin enables Progressive Web App + Offline functionality
 		// To learn more, visit: https://gatsby.dev/offline
 		// `gatsby-plugin-offline`,
